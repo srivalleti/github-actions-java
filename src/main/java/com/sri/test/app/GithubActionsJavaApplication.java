7cdc8832caa6dbe921772a4362bf391f3797956c
Srinivasulu Valleti <sri.valleti@gmail.com>
2025-01-26 12:06:38 -0500

    Add files via upload

---
+++ src/main/java/com/sri/test/app/GithubActionsJavaApplication.java
@@ -0,0 +1,13 @@
+package com.sri.test.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionsJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubActionsJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sri/test/app/GithubActionsJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sri.test.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionsJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
